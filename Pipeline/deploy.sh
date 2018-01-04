a714acb530b96ae23849534ed3b3703370a449ed
Dean <Andrew.Dean@thomsonreuters.com>
2018-01-04 16:07:18 +0000

    Added extra push with build number as well as latest tag

--- Pipeline/deploy.sh
+++ Pipeline/deploy.sh
@@ -6,6 +6,7 @@ function deployFail {
 trap deployFail ERR
 
 docker tag jowi:latest 015887481462.dkr.ecr.us-east-1.amazonaws.com/acuna-jowi:latest
+docker tag 015887481462.dkr.ecr.us-east-1.amazonaws.com/acuna-jowi:latest 015887481462.dkr.ecr.us-east-1.amazonaws.com/acuna-jowi:$BUILD_NUMBER
 $(aws ecr get-login --no-include-email --region us-east-1)
 docker push 015887481462.dkr.ecr.us-east-1.amazonaws.com/acuna-jowi:latest
 docker push 015887481462.dkr.ecr.us-east-1.amazonaws.com/acuna-jowi:$BUILD_NUMBER
