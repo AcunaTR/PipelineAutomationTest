61da8ca22f4ac1b474ed6656e5d920b606f97f4d
Dean <Andrew.Dean@thomsonreuters.com>
2018-01-08 10:01:01 +0000

    Found correct junit version

--- Janra.Jowi/src/test/java/AllJowiTests.java
+++ Janra.Jowi/src/test/java/AllJowiTests.java
@@ -1,27 +1,17 @@
 package test.java;
-import static org.junit.Assert.*;
-
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 import test.java.Network.IncomingRequestHandlerErrorTests;
 
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+	IncomingRequestHandlerErrorTests.class
+	
+
+})
+
 public class AllJowiTests {
-/*
-	@RunWith(Suite.class)
-	@Suite.SuiteClasses({
-		IncomingRequestHandlerErrorTests.class,
-		ConfigurationTestSuite.class,
-		ProtocolBuildersTestSuite.class
-		
-		
 
-	})
-*/
-	public class TestAll {
-
-
-	}
 }
