baf9b7cb6726bdd58b43dcc6074e09404f6db194
Dean <Andrew.Dean@thomsonreuters.com>
2018-01-25 16:00:19 +0000

    Automation config

--- Pipeline/build.sh
+++ Pipeline/build.sh
@@ -1,6 +1,4 @@
-CURDIR=`pwd`
-
 docker run --rm \
-           -v "${CURDIR}/:/build" \
+           -v "pwd/:/build" \
            --workdir /build/Janra.Jowi \
            maven:3.5.2-jdk-8 mvn compile
