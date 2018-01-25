8962b949a8a95fc5fa784579679b228046e41aa8
Dean <Andrew.Dean@thomsonreuters.com>
2018-01-25 14:50:02 +0000

    Initial commit

--- Pipeline/deploy.sh
+++ Pipeline/deploy.sh
@@ -4,11 +4,3 @@ function deployFail {
     echo "Deploy to AWS Lambda failed"
 }
 trap deployFail ERR
-
-VERSION=$(cat ./version)
-
-docker tag jowi:latest 015887481462.dkr.ecr.us-east-1.amazonaws.com/acuna-jowi:latest
-docker tag 015887481462.dkr.ecr.us-east-1.amazonaws.com/acuna-jowi:latest 015887481462.dkr.ecr.us-east-1.amazonaws.com/acuna-jowi:$VERSION.$BUILD_NUMBER
-$(aws ecr get-login --no-include-email --region us-east-1)
-docker push 015887481462.dkr.ecr.us-east-1.amazonaws.com/acuna-jowi:latest
-docker push 015887481462.dkr.ecr.us-east-1.amazonaws.com/acuna-jowi:$VERSION.$BUILD_NUMBER
